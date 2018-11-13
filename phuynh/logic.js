2081fbe03f1e5410a4d6e8509593809d1d4c5803
SgtStarshine <starshine621@ymail.com>
2018-11-13 08:51:16 -0500

    added logic to create a list using data from Firebase

--- phuynh/logic.js
+++ phuynh/logic.js
@@ -1,6 +1,4 @@
-$(document).ready( function () {
-
-    var config = {
+var config = {
         apiKey: "AIzaSyAGAhaC7nIXYdXfql1p3bpjPT8vCfQaAxQ",
         authDomain: "project1-test-triptracker.firebaseapp.com",
         databaseURL: "https://project1-test-triptracker.firebaseio.com",
@@ -9,8 +7,18 @@ $(document).ready( function () {
         messagingSenderId: "158304705795"
     };
     firebase.initializeApp(config);
+
+$(document).ready( function () {    
+
+    var id;
+    var itinLoad = [];
+
+    var randomid = (Math.random()+' ').substring(2,10)+(Math.random()+' ').substring(2,10);
+    id = parseInt(randomid, 10);
+    console.log("id is: "+id);
+});    
     
-    
+function createDestData() {
     // Event listener for translating user input data into the Trip Planner Entries (TPE) card
     $("#entrySubmit").on("click", function(event) {
         event.preventDefault();
@@ -36,13 +44,24 @@ $(document).ready( function () {
         var newEntry = $("<tr>");
         $("#newEntryList2").append(newEntry);
 
+        var newEntryData = {
+            dest: dest,
+            startDate: startD,
+            endDate: endD,
+            startTime: startT,
+            endTime: endT,
+            partyMembers: pMembers
+        }
+
+        database.ref('plan/' + id).push(newEntryData);
+
         // Summited info is converted to be displayed in TPE card
-        var destEntryItem = $("<th>").text(dest);
-        var sDateEntryItem = $("<th>").text(startD);
-        var sTimeEntryItem = $("<th>").text(startT);
-        var eDateEntryItem = $("<th>").text(endD);
-        var eTimeEntryItem = $("<th>").text(endT);
-        var pMembersEntryItem = $("<th>").text(pMembers);
+        var destEntryItem = $("<td>").text(newEntryData.dest);
+        var sDateEntryItem = $("<td>").text(newEntryData.startD);
+        var sTimeEntryItem = $("<td>").text(newEntryData.startT);
+        var eDateEntryItem = $("<td>").text(newEntryData.endD);
+        var eTimeEntryItem = $("<td>").text(newEntryData.endT);
+        var pMembersEntryItem = $("<td>").text(newEntryData.pMembers);
         
         destEntryItem.attr("scope", "col");
         sDateEntryItem.attr("scope", "col");
@@ -58,7 +77,59 @@ $(document).ready( function () {
         newEntry.append(eDateEntryItem);
         newEntry.append(eTimeEntryItem);
         newEntry.append(pMembersEntryItem);
-    })
 
+        $("#destInput").val('');
+        $("#datepicker1").val('');
+        $("#datepicker2").val('');
+        $("#timepicker1").val('');
+        $("#timepicker2").val('');
+        $("#party-members").val('');
+    });    
+};
 
-});
+function loadEntry() {
+
+    $("#load-entry").on("click", function(event) {
+        event.preventDefault();
+
+        // Pulls the existing itinerary ID out of the text input and assigns it to a variable.
+        id  = $("#ID-Input").val().trim();
+        console.log(id);
+        // Do a one-time read from Firebase, reading the destination objects that were created/written to Firebase on the "itinerary builder" page.  
+        database.ref('plan/' + id + '/').once('value').then(function(snapshot){
+            console.log(snapshot.val());
+            itinLoad = snapshot.val();
+            console.log('---------');
+
+            // Since each individual destination returns as an individual object separately from one another, we need to convert that list of objects to an array in order to loop through it and pull the data from each one, in order to propagate our table.
+            var itinArray = Object.values(itinLoad)
+            console.log("Number of objects in itinerary array: " + itinArray.length);
+            console.log(itinArray);
+
+            var itinList = $("<ul></ul>");
+
+            for (var i = 0; i < itinArray.length; i++) {
+                console.log(itinArray[i].city);
+                console.log(itinArray[i].startDate);
+                console.log(itinArray[i].endDate);
+                console.log('-------------');
+            }
+            for (var i = 0; i < itinArray.length; i++) {
+                var itinListItem = $("<li></li>");
+                var itinA = $("<a></a>");
+
+                itinList.addClass("ui-menu-item");
+                itinList.attr("role", "menuitem");
+
+                itinA.addClass("ui-all");
+                itinA.attr("tabindex", "-1");
+
+                itinA.text(itinArray[i].city);
+                itinListItem.append(itinA);
+
+                itinList.append(itinListItem);
+            }
+            $("#").append(itinList);
+        });
+    });
+};
